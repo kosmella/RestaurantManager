831345102e4a7707b18a990f75f180e8a3bb31b7
kosmella <kosmella@gmail.com>
2016-07-08 19:52:21 -0500

    Module 3 homework
    Completed module 3

--- RestaurantManager/RestaurantManager/MainPage.xaml.cs
+++ RestaurantManager/RestaurantManager/MainPage.xaml.cs
@@ -26,5 +26,15 @@ namespace RestaurantManager
         {
             this.InitializeComponent();
         }
+
+        private void OrderPage_Click(object sender, RoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(OrderPage));
+        }
+
+        private void ExpeditePage_Click(object sender, RoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(ExpeditePage));
+        }
     }
 }
